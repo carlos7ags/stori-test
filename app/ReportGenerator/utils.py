3277601fbd7a371b2f3a850959a5e20e2d64a335
Carlos Munoz <carlos7ags@gmail.com>
2022-08-26 15:46:04 -0500

    add summary metrics calculators

--- app/ReportGenerator/utils.py
+++ app/ReportGenerator/utils.py
@@ -1,3 +1,4 @@
+from typing import List, Dict
 from app.ReportGenerator.transaction import Transaction
 
 
@@ -10,3 +11,29 @@ def transaction_parser(transaction: str):
         type="credit" if txn[2][0] == "+" else "debit",
         value=float(txn[2][1:]),
     )
+
+
+def get_average_value(transactions: List[Transaction]) -> float:
+    return sum([txn.value for txn in transactions]) / len(transactions)
+
+
+def get_total_balance(transactions: List[Transaction]) -> float:
+    return sum([txn.value if "credit" else -txn.value for txn in transactions])
+
+
+def get_transactions_summary(transactions: List[Transaction]) -> Dict:
+    summary = dict()
+    summary["balance"] = get_total_balance(transactions)
+    periods = set([txn.month for txn in transactions])
+    for period in periods:
+        period_transactions = list(filter(lambda x: x.month == period, transactions))
+
+        summary[period]["transactions_count"] = len(period_transactions)
+
+        credit_transactions = list(filter(lambda x: x.type == "credit", period_transactions))
+        summary[period]["credit_average"] = get_average_value(credit_transactions)
+
+        debits_transactions = list(filter(lambda x: x.type == "debit", period_transactions))
+        summary[period]["debit_average"] = get_average_value(debits_transactions)
+
+    return summary
